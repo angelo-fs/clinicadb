97eb994d2d71e54f9c83a7c9e77e27789b3f62a4
angelo-fs <liberioangelo303@gmail.com>
2025-05-09 20:49:32 -0300

    Update delete.sql

--- dml/delete.sql
+++ dml/delete.sql
@@ -1,1 +1,9 @@
+-- Deletar agendamentos de um paciente
+DELETE FROM agendamentos
+WHERE id_paciente = 10;
 
+-- Deletar as consultas de um paciente
+DELETE FROM consultas
+WHERE id_agendamento IN (
+    SELECT id_agendamento FROM agendamentos WHERE id_paciente = 10
+);
