769eeb8c235815e16d1bfca5697b10f0e8823b1d
angelo-fs <liberioangelo303@gmail.com>
2025-05-09 20:57:23 -0300

    Update update.sql

--- dml/update.sql
+++ dml/update.sql
@@ -1,1 +1,9 @@
+-- Atualiza o status do agendamento para 'Concluído'
+UPDATE agendamentos
+SET status = 'Concluído'
+WHERE id_agendamento = 1001;
 
+-- Atualiza a observação da consulta relacionada
+UPDATE consultas
+SET observacoes = 'Consulta realizada com sucesso. Paciente estável.'
+WHERE id_agendamento = 1001;
